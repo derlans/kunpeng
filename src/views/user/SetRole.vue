2a9f3d551c09f349f0ae9e989ad72cf07c3cb0c9
derlan <1372545550@qq.com>
2022-03-27 17:21:37 +0800

    reactive->ref

--- src/views/user/SetRole.vue
+++ src/views/user/SetRole.vue
@@ -20,27 +20,27 @@
   import { useRoleStore } from '@/store/modules/role';
   import { setUserRoles } from '@/api/user/index';
   import { User } from './index';
-  import { reactive } from 'vue';
+  import { ref } from 'vue';
   const roleStore = useRoleStore();
   const Props = defineProps<{
     user: User;
   }>();
 
-  const sysRoleids = reactive(Props.user.sysRoles.map((role) => role.id));
+  const sysRoleids = ref(Props.user.sysRoles.map((role) => role.id));
   const Emit = defineEmits<{
     (e: 'update:sysRoles', user: User['sysRoles']): void;
   }>();
   async function handelUpdate(v) {
-    sysRoleids.length = 0;
-    sysRoleids.push(...v);
+    sysRoleids.value = v;
   }
   async function updateRoles() {
     try {
-      await setUserRoles(Props.user.id, sysRoleids);
+      await setUserRoles(Props.user.id, sysRoleids.value);
       Emit(
         'update:sysRoles',
-        roleStore.roles.filter((role) => sysRoleids.includes(role.id))
+        roleStore.roles.filter((role) => sysRoleids.value.includes(role.id))
       );
+      window['$message'].success('更新成功');
     } catch (error) {
       window['$message'].error('更新失败');
     }
