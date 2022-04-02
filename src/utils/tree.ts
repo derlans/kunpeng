34a059feaaa204eb898a63ee151a48281537c6c4
derlan <1372545550@qq.com>
2022-04-02 12:43:51 +0800

    feat:utils of tree

---
+++ src/utils/tree.ts
@@ -0,0 +1,72 @@
+export function dfs<T>(
+  nodes: T[],
+  value: any,
+  key = 'key',
+  childrenKey = 'children'
+): T | undefined {
+  for (const node of nodes) {
+    if (node[key] === value) {
+      return node;
+    }
+    if (node[childrenKey] && node[childrenKey].length) {
+      const result = dfs(node[childrenKey], value, key, childrenKey);
+      if (result) {
+        return result as T;
+      }
+    }
+  }
+}
+
+export function searchParentNode<T>(
+  nodes: T[],
+  value: any,
+  key = 'key',
+  childrenKey = 'children',
+  parentNode: T | undefined = undefined
+): T | undefined {
+  for (const node of nodes) {
+    if (node[key] === value) {
+      return parentNode;
+    }
+    parentNode = node;
+    if (node[childrenKey] && node[childrenKey].length) {
+      const result = searchParentNode(
+        node[childrenKey] as T[],
+        value,
+        key,
+        childrenKey,
+        parentNode
+      );
+      if (result) {
+        return result as T;
+      }
+    }
+  }
+}
+
+export function searchParentNodes<T>(
+  nodes: T[],
+  value: any[],
+  key = 'key',
+  childrenKey = 'children',
+  parentNode: T | undefined = undefined
+): T[] {
+  const parentNodes: T[] = [];
+  for (const node of nodes) {
+    if (node[key] === value) {
+      parentNodes.push(node);
+    }
+    parentNode = node;
+    if (node[childrenKey] && node[childrenKey].length) {
+      const result = searchParentNodes(
+        node[childrenKey] as T[],
+        value,
+        key,
+        childrenKey,
+        parentNode
+      );
+      parentNodes.push(...result);
+    }
+  }
+  return parentNodes;
+}
